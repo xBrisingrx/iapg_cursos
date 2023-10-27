c34840463be37da94f73e17dbd39cee65bb7919d
Mauro Almiron <mdavid.almiron@gmail.com>
2023-10-27 19:51:30 -0300

    added: helper to set a current class in navbar

---
+++ app/helpers/link_helper.rb
@@ -0,0 +1,9 @@
+module LinkHelper
+  def nav_link_class(path)
+    if current_page?(path)
+      "bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+    else
+      "text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+    end
+  end
+end
