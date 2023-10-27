bf77fb1e31a3518177f62afd2d37028f7da49eae
Mauro Almiron <mdavid.almiron@gmail.com>
2023-10-27 00:12:09 -0300

    added js libraries
    I use this libraries for common interactions like show a notification or make a select with filter

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,6 +2,11 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.2/dist/stimulus.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "el-transition", to: "https://ga.jspm.io/npm:el-transition@0.0.7/index.js"
+pin "tom-select", to: "https://ga.jspm.io/npm:tom-select@2.2.3/dist/js/tom-select.complete.js"
+pin "stimulus-notification", to: "https://ga.jspm.io/npm:stimulus-notification@2.2.0/dist/stimulus-notification.mjs"
+pin "hotkeys-js", to: "https://ga.jspm.io/npm:hotkeys-js@3.12.0/dist/hotkeys.esm.js"
+pin "stimulus-use", to: "https://ga.jspm.io/npm:stimulus-use@0.51.3/dist/index.js"
